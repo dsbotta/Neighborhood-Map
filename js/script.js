aabd4f7d0d8ce48beddb8904a984ed7b6c2ae2f1
dsbotta <danielbotta@Daniels-MacBook-Pro.local>
2015-03-07 20:49:33 -0500

    Added styles and visibility control nav bar/hide nav bar/hide & show markers

--- js/script.js
+++ js/script.js
@@ -1,18 +1,5 @@
-
 var map;
-function initialize() {
-      
-  var mapOptions = {
-    zoom: 15,
-    center: new google.maps.LatLng(38.893952, -77.029613)
-  };
-  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-  infowindow = new google.maps.InfoWindow({
-      content: null
-    });  
-
-setMarkers(markers);
-}
+var markersArray = [];
 
 function loadScript() {
   var script = document.createElement('script');
@@ -23,130 +10,194 @@ function loadScript() {
 }
 window.onload = loadScript;
 
-//Holds the different street view images for each marker
-var flickrPicArray = [];    
 
-    var markers = [
-        {   
-        title: "The Thomas Jefferson Memorial",
-        lat: 38.881004, 
-        lng: -77.036463,
-        map: map,
-        streetAddress: "701 East Basin SW",
-        cityAddress: "Washington, DC 20242",
-        id: "nav0",
-        visible: ko.observable(true)
-        },
-        {   
-        title: "The Lincoln Memorial",
-        lat: 38.889269, 
-        lng: -77.050176,
-        map: map,
-        streetAddress: "2 Lincoln Memorial Cir",
-        cityAddress: "Washington, DC 20037",
-        id: "nav1",
-        visible: ko.observable(false)
-        },
-        {   
-        title: "The Washington Monument",
-        lat: 38.889484, 
-        lng: -77.0363733,
-        map: map,
-        streetAddress: "2 15th St NW",
-        cityAddress: "Washington, DC 20007",
-        id: "nav2",
-        visible: ko.observable(true)
-        },
-        {   
-        title: "The United States Capital",
-        lat: 38.889939, 
-        lng: -77.00905,
-        map: map,
-        streetAddress: "East Capitol St NE & First St SE",
-        cityAddress: "Washington, DC 20004",
-        id: "nav3",
-        visible: ko.observable(true)
-        },
-        {
-        title: "The White House",
-        lat: 38.8989013, 
-        lng: -77.0324048,
-        map: map,
-        streetAddress: "1600 Pennsylvania Ave NW SW",
-        cityAddress: "Washington, DC 20500",
-        id: "nav4",
-        visible: ko.observable(true)
-        },
-        {   
-        title: "The National WWII Memorial",
-        lat: 38.889443, 
-        lng: -77.040556,
-        map: map,
-        streetAddress: "1750 Independence Ave SW",
-        cityAddress: "Washington, DC 20006",
-        id: "nav5",
-        visible: ko.observable(true)
-        }   
-    ];
+function initialize() {  
+  var mapOptions = {
+    zoom: 15,
+    center: new google.maps.LatLng(38.893952, -77.029613)
+  };
+  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);  
 
+setMarkers(markers);
 
+setAllMap();
+}
 
-    var viewModel = {
-        query: ko.observable(''),
-    };
+function setAllMap() {
+  for (var i = 0; i < markers.length; i++) {
+    if(markers[i].boolTest == true) {
+    markers[i].holdMarker.setMap(map);
+    } else {
+    markers[i].holdMarker.setMap(null);
+    }
+  }
+};
 
-    viewModel.markers = ko.dependentObservable(function() {
-        var search = this.query().toLowerCase();
-        return ko.utils.arrayFilter(markers, function(marker) {
-        if (marker.title.toLowerCase().indexOf(search) >= 0) {
-                return marker.visible(true)
-            } else {
-                return marker.visible(false)
-            }
-        });       
-    }, viewModel);
+var markers = [
+    {   
+    title: "The Thomas Jefferson Memorial",
+    lat: 38.881004, 
+    lng: -77.036463,
+    streetAddress: "701 East Basin SW",
+    cityAddress: "Washington, DC 20242",
+    id: "nav0",
+    visible: ko.observable(true),
+    boolTest: true
+    },
+    {   
+    title: "The Lincoln Memorial",
+    lat: 38.889269, 
+    lng: -77.050176,
+    streetAddress: "2 Lincoln Memorial Cir",
+    cityAddress: "Washington, DC 20037",
+    id: "nav1",
+    visible: ko.observable(true),
+    boolTest: true
+    },
+    {   
+    title: "The Washington Monument",
+    lat: 38.889484, 
+    lng: -77.0363733,
+    streetAddress: "2 15th St NW",
+    cityAddress: "Washington, DC 20007",
+    id: "nav2",
+    visible: ko.observable(true),
+    boolTest: true
+    },
+    {   
+    title: "The United States Capital",
+    lat: 38.889939, 
+    lng: -77.00905,
+    streetAddress: "East Capitol St NE & First St SE",
+    cityAddress: "Washington, DC 20004",
+    id: "nav3",
+    visible: ko.observable(true),
+    boolTest: true
+    },
+    {
+    title: "The White House",
+    lat: 38.8989013, 
+    lng: -77.0324048,
+    streetAddress: "1600 Pennsylvania Ave NW SW",
+    cityAddress: "Washington, DC 20500",
+    id: "nav4",
+    visible: ko.observable(true),
+    boolTest: true
+    },
+    {   
+    title: "The National WWII Memorial",
+    lat: 38.889443, 
+    lng: -77.040556,
+    streetAddress: "1750 Independence Ave SW",
+    cityAddress: "Washington, DC 20006",
+    id: "nav5",
+    visible: ko.observable(true),
+    boolTest: true
+    },
+    {
+    title: "National Gallery of Art",
+    lat: 38.891298, 
+    lng: -77.019965,
+    streetAddress: "6th & Constitution Ave NW",
+    cityAddress: "Washington, DC 20565",
+    id: "nav6",
+    visible: ko.observable(true),
+    boolTest: true
+    },
+    {
+    title: "Smithsonian National Museum of Natural History",
+    lat: 38.891266, 
+    lng: -77.026065,
+    streetAddress: "10th St. & Constitution Ave. NW",
+    cityAddress: "Washington, DC 20560",
+    id: "nav7",
+    visible: ko.observable(true),
+    boolTest: true
+    },
+    {
+    title: "Arts and Industries Building",
+    lat: 38.888632, 
+    lng: -77.024372,
+    streetAddress: "900 Jefferson Dr SW",
+    cityAddress: "Washington, DC 20560",
+    id: "nav8",
+    visible: ko.observable(true),
+    boolTest: true
+    }   
+];
 
-    ko.applyBindings(viewModel);
+// var mapImage = {
+//     image: {
+//         url: 'img/marker.png',
+//         size: new google.maps.Size(14, 30),
+//         origin: new google.maps.Point(0, 0),
+//         anchor: new google.maps.Point(6, 28)
+//     },
+//     shape: {
+//         coords: [1,1,13,1,13,29,1,29],
+//         type: 'poly'
+//       },
+// };
+
+// $("#button").click(function() {
+//     for (var n in markers) {
+//     markers[n].imageUrl = 'img/blank-marker.png';
+//     };
+//     setMarkers(markers);
+// });
+
+    // var viewModel = {
+    //     query: ko.observable(''),
+    // };
+
+    // viewModel.markers = ko.dependentObservable(function() {
+    //     var search = this.query().toLowerCase();
+    //     return ko.utils.arrayFilter(markers, function(marker) {
+    //     if (marker.title.toLowerCase().indexOf(search) >= 0) {
+    //             return marker.visible(true)
+    //         } else {
+    //             clearMarkers()
+    //             return marker.visible(false)
+    //         }
+    //     });       
+    // }, viewModel);
+
+    // ko.applyBindings(viewModel);
+
 
 
 function setMarkers(location) {
-    var markers = new Array();
+    
     for(i=0; i<location.length; i++) {
-        var marker = new google.maps.Marker({
+        location[i].holdMarker = new google.maps.Marker({
           position: new google.maps.LatLng(location[i].lat, location[i].lng),
           map: map,
-          title: location[i].title  
+          title: location[i].title,
+          icon: {
+            url: 'img/marker.png',
+            size: new google.maps.Size(25, 40),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(12.5, 40)
+            },
+          shape: {
+            coords: [1,25,-40,-25,1],
+            type: 'poly'
+          }  
         });
 
-        // var flickrPicSearch = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=5a0f379fef5e197fc8e6dbcc7a4e444d&text=" + 
-        //                       markers[i].title.replace(/ /g, "+") + 
-        //                       "&accuracy=16&content_type=1&lat=" + 
-        //                       markers[i].lat + "&lon=" + 
-        //                       markers[i].lng + 
-        //                       "&format=json&nojsoncallback=1";
-        // var x = 0;
-        // $.getJSON(flickrPicSearch, function (data) {
-        //     for(x in data.photos) {
-        //         var image = data.photos;
-        //         var location = markers;
-        //         flickrPicArray.push(image[x]); //Captures all json data for each location search
-        //     // var randomPic = Math.floor((Math.random() * image.length -1) + 1);
-        //     // location[x].contentString = '<img src="http://farm' + image[randomPic].farm + '.static.flickr.com/' + image[randomPic].server + '/' + image[randomPic].id +'_' + image[randomPic].secret + '_m.jpg"><p>' + mapModel.markers[x].streetAddress + '<br>' + mapModel.markers[x].cityAddress + '</p>';
-        //     };
-        // });
-
-        location[i].contentString = '<p>' + location[i].streetAddress + '<br>' + location[i].cityAddress + '</p>';
-        markers.push(marker);
+        location[i].contentString = '<strong>' + location[i].title + '</strong><br><p>' + location[i].streetAddress + '<br>' + location[i].cityAddress + '</p>';
+        // markersArray.push(marker);
+        // markersArray.push(location[i].holdMarker);
 
         var infowindow = new google.maps.InfoWindow({
             content: markers[i].contentString
         });
-        new google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        new google.maps.event.addListener(location[i].holdMarker, 'click', (function(marker, i) {
           return function() {
             infowindow.setContent(location[i].contentString);
             infowindow.open(map,this);
           } 
-        })(marker, i));
+        })(location[i].holdMarker, i));
         // $("ul").append('<a id="nav' + i + '" data-bind="visible: showContent" href="#"><li>' + mapModel.markers[i].title + '</li></a><hr>');
         
         var searchNav = $('#nav' + i);
@@ -156,6 +207,58 @@ function setMarkers(location) {
             infowindow.setContent(location[i].contentString);
             infowindow.open(map,marker);
           } 
-        })(marker, i));
+        })(location[i].holdMarker, i));
     }
 };
+
+var viewModel = {
+    query: ko.observable(''),
+};
+
+viewModel.markers = ko.dependentObservable(function() {
+    var self = this;
+    var search = self.query().toLowerCase();
+    return ko.utils.arrayFilter(markers, function(marker) {
+    if (marker.title.toLowerCase().indexOf(search) >= 0) {
+            marker.boolTest = true;
+            return marker.visible(true)
+        } else {
+            marker.boolTest = false;
+            setAllMap();
+            return marker.visible(false)
+        }
+    });       
+}, viewModel);
+
+ko.applyBindings(viewModel);
+
+$("#input").keyup(function() {
+setAllMap();
+});
+
+var isNavVisible = true;
+function hideNav() {
+    if(isNavVisible === true) {
+            $("#search-nav").animate({
+                height: 0, 
+            }, 500);
+            setTimeout(function() {
+                $("#search-nav").hide();
+            }, 500);    
+            $("#arrow").attr("src", "img/down-arrow.gif");
+            isNavVisible = false;
+            
+    } else {
+            $("#search-nav").show();
+            $("#search-nav").animate({
+                height: 493, 
+            }, 500);
+            $("#arrow").attr("src", "img/up-arrow.gif");
+            isNavVisible = true;  
+    };
+};
+
+$("#arrow").click(hideNav);    
+
+
+
